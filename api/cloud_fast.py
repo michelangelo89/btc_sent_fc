94da3e0b3b114d9a86b74eeaf4e43e55f791287e
Sergey Sudakov <sergey19sudakov@gmail.com>
2021-11-25 14:57:30 +0000

    Pre-merge

--- api/cloud_fast.py
+++ api/cloud_fast.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from CloudSentiment.cloud_trainer import Sentimenter
+from CloudSentiment.cloud_data import get_data, transform_data
 from datetime import datetime
 import pytz
 import joblib
@@ -26,12 +27,13 @@ def index():
     return {"greeting": "Hello world"}
 
 @app.get("/predict")
-def predict(date_list,
-            text_list,
+def predict(N = None,
             out_name = "test_api.csv"):
-
-    text_list = ast.literal_eval(text_list)
-    date_list = ast.literal_eval(date_list)
+    if N:
+        N = int(N)
+    df = get_data("crypto", nrows = N, how = "google")
+    text_list, date_list = transform_data(df)
+    #date_list = ast.literal_eval(date_list)
     sentiment = Sentimenter(date_list, text_list, out_name = out_name)
     sentiment.set_model()
     sentiment.run()

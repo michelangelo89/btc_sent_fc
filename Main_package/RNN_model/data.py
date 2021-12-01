f65407060a41c22b9cf209469b30ea14eab43138
Sergey Sudakov <sergey19sudakov@gmail.com>
2021-12-01 16:41:06 +0000

    Pipeline stuff

--- Main_package/RNN_model/data.py
+++ Main_package/RNN_model/data.py
@@ -57,7 +57,9 @@ def get_features_from_raw_data():
 
 def clean_features(df):
 
-    log = FunctionTransformer(lambda x: np.log(x))
+    log = FunctionTransformer(np.log)
+    #def log(x):
+    #    return np.log(x)
 
     log_col = Pipeline([('log',log),('imputer', KNNImputer()),('scaler', StandardScaler())])
 
@@ -65,24 +67,22 @@ def clean_features(df):
 
     no_log_col = Pipeline([('imputer', KNNImputer()),('scaler', StandardScaler())])
 
-    columns = make_column_transformer(
+    preproc_pipe = make_column_transformer(
         (log_col, log_col_),
         (no_log_col, no_log_col_),
         (target_col, target),
         remainder= no_log_col)
     
-    preproc_pipe = Pipeline(["columns", columns])
+    #preproc_pipe = Pipeline(["columns", columns])
     preproc_pipe.fit(df)
-    #joblib.dump(preproc_pipe, "../pipe.joblib")
-    
-    return preproc_pipe#pd.DataFrame(preproc_pipe.transform(df),
-                       # columns = df.columns,
-                       # index = df.index)
+    joblib.dump(preproc_pipe, "../pipe.joblib")
+
+    return pd.DataFrame(preproc_pipe.transform(df),
+                       columns = df.columns,
+                       index = df.index)
 
 def clean_test_features(df):
-    with open('../pipeline.pickle', 'rb') as picklefile:
-        preproc_pipe = pickle.load(picklefile)
-    #preproc_pipe = joblib.load("../pipeline.joblib")
+    preproc_pipe = joblib.load("../pipe.joblib")
     return pd.DataFrame(preproc_pipe.transform(df),
                         columns = df.columns,
                         index = df.index)
